c4e6176c3db787f625c04915b8aaac20de7054c3
Nikita <n.chursin.ed@gmail.com>
2025-05-07 16:29:29 +0300

    Fix import

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
 import pytest
-import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 @pytest.fixture
 def client():
